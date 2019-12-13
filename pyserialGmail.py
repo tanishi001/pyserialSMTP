c58a6a3d88ccf22a4633054838370977791d46eb
Alice18003 <reki3316@gmail.com>
2019-12-13 13:11:28 +0900

    first commit

---
+++ pyserialGmail.py
@@ -0,0 +1,44 @@
+import serial
+import smtplib
+import datetime
+import sys
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+normal = 8
+timeMin = '09'
+timeMax = '22'
+
+smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+smtpobj.ehlo()
+smtpobj.starttls()
+smtpobj.ehlo()
+smtpobj.ehlo()
+smtpobj.login("from_gmailaddress", sys.argv[1]) #sys.argv[1] is gmail's password
+
+ser = serial.Serial('/dev/ttyS5', 9600)
+while True:
+    dt_now = datetime.datetime.now()
+    data  = ser.read()
+    value = int.from_bytes(data, 'big') - 48
+    hour = de_now.strftime('%H')
+    if value < normal and hour == timeMin and hour== timeMax:
+        ser.write(b"1");
+        
+        msg = MIMEText('WORNIG!!! Someone invaded!')
+        msg['subject'] = 'WORNIG!!!'
+        msg['From'] = 'from_gmailaddress'
+        msg['To'] = 'to_mailaddress'
+        msg['Date'] = formatdate()
+        smtpobj.sendmail("from_gmailaddress", "to_mailaddress", msg.as_string())
+    elif value < normal and hour != timeMin or hour != timeMax:
+        ser.write(b"0");
+        
+        msg = MIMEText('Someone enter or exit')
+        msg['subject'] = 'notification'
+        msg['From'] = 'from_gmailaddress'
+        msg['To'] = 'to_mailaddress'
+        msg['Date'] = formatdate()
+        smtpobj.sendmail("from_gmailaddress", "to_mailaddress", msg.as_string())
+smtpobj.close()
+ser.close()
